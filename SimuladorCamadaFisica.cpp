948b9146d70d68711db3e604da5eab38d1c571e2
Letícia de Souza <42966141+leticia-s@users.noreply.github.com>
2019-06-17 22:57:50 -0300

    Comentarios + digitar tipo de codificação

--- SimuladorCamadaFisica.cpp
+++ SimuladorCamadaFisica.cpp
@@ -37,7 +37,8 @@ string CamadaFisicaReceptoraDecodificacaoManchesterDiferencial(string quadro);
 void CamadaDeAplicacaoReceptora (string quadro );
 void AplicacaoReceptora(string mensagem);
 
-
+//variavel global para o tipo de codificacao
+int tipo_de_codific;
 
 int main () {
 AplicacaoTransmissora();
@@ -46,8 +47,8 @@ AplicacaoTransmissora();
 string lerStringArquivo(string mensagem){
 	char caracter;
 	FILE *ptr;
-	//ler Arquivo txt
-	ptr = fopen("text_string.txt","r"); //colocar nome do arquivo txt para ser lido
+	//ler Arquivo txt << tudo no arquivo txt sera somente uma unica string
+	ptr = fopen("text_string.txt","r"); //colocar nome do arquivo .txt para ser lido
 	if (ptr == NULL){
     	printf("Problemas na abertura do arquivo\n");
     	exit(0);
@@ -55,16 +56,27 @@ string lerStringArquivo(string mensagem){
 	
 	while (!feof(ptr)){   	
 			fread(&caracter,sizeof(char),1,ptr); // ler caracter por caracter
-			mensagem += caracter;
+			mensagem += caracter; // adiciona caracter na string
 	}
     fclose(ptr);
     return mensagem;
-};
+};//fim do metodo lerStringArquivo
+
 void AplicacaoTransmissora (void) {
 	string mensagem;
+	tipo_de_codific = -1; //valor inicial pra condicao while
+	//while para selecionar o tipo de codificacao
+	while(tipo_de_codific != 0 && tipo_de_codific != 1 && tipo_de_codific != 2 ){
+		cout << "Digite o tipo de codificacao:\n    0- Binaria\n    1- Manchester\n    2- Manchester Diferencial\n>>";
+		cin >> tipo_de_codific; 
+		cin.ignore();// se for usar getline tem que limpar \n do buffer
+		if(tipo_de_codific != 0 && tipo_de_codific != 1 && tipo_de_codific != 2 )
+			cout << "tipo de codificacao invalida!" << endl;
+	}
+	//ler de um arquivo
+	mensagem += lerStringArquivo(mensagem); 
 	//cout << "Digite uma mensagem:" << endl;
     //getline(cin, mensagem); // cin sozinho nao pega espacos
-    mensagem += lerStringArquivo(mensagem); 
 	//chama a proxima camada
 	CamadaDeAplicacaoTransmissora(mensagem); //em um exemplo mais realistico, aqui seria dado um SEND do SOCKET
 }//fim do metodo AplicacaoTransmissora
@@ -84,7 +96,7 @@ void CamadaDeAplicacaoTransmissora (string mensagem) {
 
 
 void CamadaFisicaTransmissora (string quadro) {
-	int tipoDeCodificacao = 2; //alterar de acordo o teste
+	int tipoDeCodificacao = tipo_de_codific; //alterar de acordo o teste
 	string fluxoBrutoDeBits; //ATENCAO: trabalhar com BITS!!!
 	switch (tipoDeCodificacao) {
 		case 0 : //codificao binaria
@@ -110,7 +122,7 @@ void CamadaFisicaTransmissora (string quadro) {
 
 
 string CamadaFisicaTransmissoraCodificacaoBinaria (string quadro) {
-	 cout << "Mensagem codificada em bits:  "  << quadro << endl;
+	 cout << "Mensagem codificada em bits:\n"  << quadro << endl;
     return quadro;
 }//fim do metodo CamadaFisicaTransmissoraCodificacaoBinaria
 
@@ -118,21 +130,22 @@ string CamadaFisicaTransmissoraCodificacaoBinaria (string quadro) {
 string CamadaFisicaTransmissoraCodificacaoManchester (string quadro) {
 	
 	string codificada;
-	int t = 0;
+	int t = 0; //tempo do clock iniciando com 0
 	unsigned int indice = 0;
 
 	while(quadro[indice] != '\0'){
-
+		//cada bit do quadro compara com 2 tempos de clock
 		for(t=0;t!=2;t++){
+		// um XOR do dos bits do quadro com o tempo do clock
 			if (quadro[indice] == '1' && t== 0) codificada += '1';
 			if (quadro[indice] == '1' && t== 1) codificada += '0';
 			if (quadro[indice] == '0' && t== 1) codificada += '1';
 			if (quadro[indice] == '0' && t== 0) codificada += '0';
-
+           
 		}
-		indice += 1;
+		indice += 1; // vai pro proximo bit do quadro
 	}
-cout << "Mensagem codificada (Manchester):  " << codificada << endl;
+cout << "Mensagem codificada (Manchester):\n" << codificada << endl;
 
 return codificada;
 
@@ -143,31 +156,32 @@ string CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(string quadro) {
 	
 	string codificada;
 	unsigned int indice = 0;
-
+    //se primeiro bit do quadro for 0
 	if (quadro[indice] == '1'){ 
 		codificada += '1';
 		codificada += '0';
 	}
-
+    //se primeiro bit do quadro for 1
 	if (quadro[indice] == '0'){ 
 		codificada += '0';
 		codificada += '1';
 	}
 	indice++;
-
+    //proximos bits
 	while(quadro[indice] != '\0'){
-
+            // se bit do quadro igual ao ultimo bit codificado
 			if (quadro[indice] == codificada[(indice*2)-1]){
 				codificada += '1';
 				codificada += '0';
 			} 
+			//se bit do quadro diferente do ultimo bit codificado
 			if (quadro[indice] != codificada[(indice*2)-1]){
 				codificada += '0';
 				codificada += '1';
 			}
 		indice += 1;
 	}
-cout << "Mensagem codificada (Manchester Diferencial):  "  << codificada << endl;
+cout << "Mensagem codificada (Manchester Diferencial):\n"  << codificada << endl;
 
 return codificada;
 
@@ -195,7 +209,7 @@ void MeioDeComunicacao (string fluxoBrutoDeBits) {
 
 
 void CamadaFisicaReceptora (string quadro) {
-	int tipoDeDecodificacao = 2; //alterar de acordo o teste
+	int tipoDeDecodificacao = tipo_de_codific; //alterar de acordo o teste
 	string fluxoBrutoDeBits; //ATENCAO: trabalhar com BITS!!!
 	switch (tipoDeDecodificacao) {
 		case 0 : //codificao binaria
@@ -227,15 +241,16 @@ string CamadaFisicaReceptoraDecodificacaoManchester (string quadro) {
 	unsigned int indice = 0;
 
 	while(quadro[indice] != '\0'){
+			//verifica os bits nas posicoes pares (0...2...4...) do quadro
 			if (quadro[indice] == '1' ) decodificada += '1';
 			if (quadro[indice] == '0' ) decodificada += '0';
 			indice+=2;
 		}
-		// Não usei clock para fazer o decodifica pq caso o quadro[Clock] seja 1 o decodifica[Clock2] é 1 tmb 
-		// Como o código de Manchester possui a lógica de a cada ciclo de clock 1 bit tranmitido será codificado para 2 sempre 01 ou 10
-		// Caso seja 10 o bit da mensagem original é 1 e caso seja 01 o bit da mensagem original é 0
-		// Isso porque eu tomei como base para o codifica que o clock começa em 0 e vai pra 1 
-		cout << "Mensagem decodificada (Manchester):  " << decodificada << endl;
+		// Nao usei clock para fazer o decodifica pq caso o quadro[Clock] seja 1 o decodifica[Clock2] e 1 tambem
+		// Como o codigo de Manchester possui a logica de a cada ciclo de clock 1 bit tranmitido sera codificado para 2 sempre 01 ou 10
+		// Caso seja 10 o bit da mensagem original sera 1 e caso seja 01 o bit da mensagem original sera 0
+		// Isso porque eu tomei como base para o codifica que o clock comeca em 0 e vai pra 1 
+		cout << "Mensagem decodificada (Manchester):\n" << decodificada << endl;
 return decodificada;
 
 	
@@ -246,27 +261,30 @@ string CamadaFisicaReceptoraDecodificacaoManchesterDiferencial(string quadro){
 	
 	string decodificada;
 	unsigned int indice = 0;
-		
+		//se primeiro bit o quadro for igual a 1
 		if (quadro[indice] == '1' ){
 				decodificada += '1';;
 		} 
+		//se primeiro bit o quadro for igual a 0
 		if (quadro[indice] == '0'){
 			decodificada += '0';
 			;
 		}
-	indice+=2;
+	indice+=2; // proxima posicao 2 do quadro
 	
-
+    // verifica restantes dos bits do quadro nas posicoes pares (0,2,4,6...) 
 	while(quadro[indice] != '\0'){
+		    //se posicao par do quadro atual for igual a posicao par do quadro anterior
 			if (quadro[indice] ==  quadro[indice-2]){
 				decodificada += '0';
 			} 
+		    //se posicao par do quadro atual for diferente a posicao par do quadro anterior
 			if (quadro[indice] != quadro[indice-2]){
 				decodificada += '1';
 			} 
-			indice+=2;
+			indice+=2; 
 		}
-		cout << "Mensagem decodificada (Manchester Diferencial):  " << decodificada << endl;
+		cout << "Mensagem decodificada (Manchester Diferencial):\n" << decodificada << endl;
 return decodificada;
 	
 }//fim do CamadaFisicaReceptoraDecodificacaoManchesterDiferencial
@@ -277,9 +295,9 @@ void CamadaDeAplicacaoReceptora (string quadro) {
 	unsigned int posCaracter = 0; 
 	//tem que pegar a cada 8 bits sera um caracter
 	while(quadro[posCaracter] != '\0'){
-		bitset<8> b(quadro.substr(posCaracter,8));
-		mensagem += b.to_ulong(); //estava trabalhando com bits
-		posCaracter += 8;
+		bitset<8> b(quadro.substr(posCaracter,8));// pega 8 bits de um caracter 
+		mensagem += b.to_ulong(); //transforma os 8 bits para um inteiro ulong e depois pra caracter
+		posCaracter += 8;// a cada posicao multiplo de 8 sera um caracter
 	}
 	//chama proxima camada
 	AplicacaoReceptora(mensagem);
@@ -287,5 +305,5 @@ void CamadaDeAplicacaoReceptora (string quadro) {
 
 
 void AplicacaoReceptora (string mensagem) {
-	cout << "A mensagem recebida foi:" << mensagem << endl;
+	cout << "A mensagem recebida foi:\n" << mensagem << endl;
 }//fim do metodo AplicacaoReceptora
